3489875d0908f95787d00d5b83bf86ea2f44ff35
Dhruv Naik <dhruviscracked@gmail.com>
2024-03-12 20:38:15 +0530

    My Posts ui improved along with the sidebar

--- src/app/(posts)/my-posts/page.tsx
+++ src/app/(posts)/my-posts/page.tsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import axios from "axios";
 import { Suspense } from "react";
+import { IoTrashBinOutline } from "react-icons/io5";
+import { CiEdit } from "react-icons/ci";
 
 interface Post {
   id: number;
@@ -32,19 +34,32 @@ export default function Posts() {
   }, [session, status]);
 
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Your Posts</h1>
+    <div className="h-full">
+      <h1 className="text-white inline border-b scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-5">
+      Your Posts...
+      </h1>
       {userPosts && userPosts.length > 0 ? (
-        <ul className="flex flex-col space-y-5 mt-5">
+        <ul className="flex flex-col space-y-5 mt-10">
           {userPosts.map((post: Post) => (
-            <div className="text-black border shadow-lg transition-transform transform hover:scale-105 p-3"
+            <div className="text-white border shadow-lg transition-transform transform hover:scale-105 p-3"
             style={{ transitionDuration: '0.3s' }}>
                 <div className="flex justify-between">
-                    <div>
-                    <li key={post.id}>{post.title}</li>
-                    <p key={post.id}>{post.content}</p>
+                    <div className="flex justify-between w-full mr-32">
+                      <div>
+                        <li className="text-xl font-bold" key={post.id}>{post.title}</li>
+                        <p key={post.id}>{post.content}</p>
+                      </div>
+                   
+                    <div className="flex justify-center items-center">
+                      <li className="text-sm font-light px-3 py-2 bg-amber-600 rounded-full rounded-tl-none">{post.category}</li>
                     </div>
-                    <li className="mr-5 text-sm font-light">{post.category}</li>
+                    
+                    </div>
+                    <div className="my-auto flex space-x-5">
+                      <button><IoTrashBinOutline size={20}/></button>
+                      <button><CiEdit size={20}/></button>
+                    </div>
+                    
                 </div>
             </div>
           ))}
--- src/components/dashboard/sidenav.tsx
+++ src/components/dashboard/sidenav.tsx
@@ -1,28 +1,47 @@
+"use client";
 import Link from 'next/link';
 import { Button } from '../ui/button';
+import Image from 'next/image';
+import GecImage from '../../../public/Images/GEC_LOGO.png';
+import { useSession } from 'next-auth/react';
+import { Space_Grotesk } from 'next/font/google';
 
+const space = Space_Grotesk({
+  weight: ['700','500'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function SideNav() {
+  const { data: session } = useSession();
+
+  // console.log(session);
+  
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
-        href="/"
-      >
-        <div className="w-32 text-white md:w-40">
-          Insider
+    <div className="bg-gray-800 flex h-full flex-col px-3 py-4 md:px-2">
+      
+        <div className='bg-white rounded-md'>
+          <Image src={GecImage}
+          alt='college-image'
+          className='rounded-md size-full'
+          />
         </div>
-      </Link>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <Link href='/' className='flex justify-center'>
-            <Button>Home</Button>
-        </Link>
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form>
+    
+      <div className="h-full flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-800 md:block">
+          <div>
+            <p className={`text-white font-light text-xl flex justify-center mt-3`}>Hello {session?.user?.username}</p>
+          </div>
+        </div>
+        <div className='flex justify-between w-full items-center mt-3'>
+          <Link href='/' className='flex justify-center'>
+              <Button>Home</Button>
+          </Link>
+          <Button variant='destructive'>Sign Out</Button>
+        </div>
+          
+      
       </div>
     </div>
   );
