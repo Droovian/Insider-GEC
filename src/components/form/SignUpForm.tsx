4e4d31684835d1e1a7b7ec9dd6b6aab05dc14586
Introvert2000 <alvareschamp200@gmail.com>
2024-03-22 03:19:20 +0530

    added boundry a bit

--- src/components/form/SignUpForm.tsx
+++ src/components/form/SignUpForm.tsx
@@ -9,13 +9,13 @@ const SignUp = () => {
         <Icons.logo className='mx-auto h-6 w-6' />
         <h1 className='text-2xl font-semibold tracking-tight'>Sign Up</h1>
         <p className='text-sm max-w-xs mx-auto'>
-          By continuing, you are setting up a Breadit account and agree to our
+          By continuing, you are setting up a Insider account and agree to our
           User Agreement and Privacy Policy.
         </p>
       </div>
       <UserAuthForm />
       <p className='px-8 text-center text-sm text-muted-foreground'>
-        Already a Breadditor?{' '}
+        Already an Insider ?{' '}
         <Link
           href='/signin'
           className='hover:text-brand text-sm underline underline-offset-4'>
