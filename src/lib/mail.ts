249937a28c1ab6c04057d3bdbc0eef812c02c1db
Dhruv Naik <dhruviscracked@gmail.com>
2024-04-17 15:41:03 +0530

    Resend Email configured & production ready

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -2,14 +2,16 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const domain = process.env.NEXT_PUBLIC_APP_URL;
+
 export const sendPasswordResetEmail = async (
   email: string,
   token: string
 ) => {
-  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+  const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@gecinsider.in",
       to: email,
       subject: "Reset your password",
       html: `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`
@@ -22,10 +24,10 @@ export const sendVerificationEmail = async (
     email: string, 
     token: string
   ) => {
-    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+    const confirmLink = `${domain}/auth/new-verification?token=${token}`;
   
     await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "mail@gecinsider.in",
       to: email,
       subject: "Confirm your email",
       html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
